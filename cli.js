357c4cff6f9592bad1a07a3385d297bb491d5e4b
David Konsumer <konsumer@jetboystudio.com>
2017-04-07 23:07:44 -0700

    simplified, added examples

--- cli.js
+++ cli.js
@@ -6,12 +6,18 @@ var client = new Blipper()
 
 const arg = require('yargs')
   .usage('Usage: $0 <command> [options]')
-  .command('name  <username>', 'Set your username')
+  .command('name  <your_username>', 'Set your username')
   .command('about <info>', 'Set your about info')
   .command('profile', 'View our username, about, and unique node id')
   .command('say <your_message>', 'Say something which your followers will see in their feeds')
   .command('add <friend_id>', 'Add unique id of another node to follow')
   .command('feed', 'Show your feed of your posts and posts by nodes you follow')
+
+  .example('$0 name konsumer', 'Set your name to "konsumer"')
+  .example('$0 about "Mostly human"', 'Set your about-text')
+  .example('$0 say "Hey planet!"', 'Say something')
+  .example('$0 add QmUNfYBRgXqV1NDDiTws9vvFWtijZwMZCu6KENEVWGcG7k', 'Follow @protometa')
+
   .help('h')
   .alias('h', 'help')
   .demandCommand(2)
@@ -24,13 +30,13 @@ about:  ${profile.about}
 id:  ${profile.id}
 `)
 
-commands.feed = client.getFeed().map(post => (`${post.author.username || post.author.id} - ${new Date(post.time)}
+commands.feed = () => client.getFeed().map(post => (`${post.author.username || post.author.id} - ${new Date(post.time)}
 ${post.content}
 `))
 
-commands.name = client.setUsername
-commands.about = client.setAbout
-commands.say = client.post
-commands.add = client.addFollowee
+commands.name = () => client.setUsername(arg.your_username)
+commands.about = () => client.setAbout(arg.info)
+commands.say = () => client.post(arg.your_message)
+commands.add = () => client.addFollowee(arg.friend_id)
 
-commands[arg._].apply(client, process.argv.slice(3)).invoke('toString').append('\n').pipe(process.stdout)
+commands[arg._]().invoke('toString').append('\n').pipe(process.stdout)
