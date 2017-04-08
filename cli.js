123f95c940e1413af68460a18df38f87bea7d307
David Konsumer <konsumer@jetboystudio.com>
2017-04-07 22:55:56 -0700

    cleaner arg parsing

--- cli.js
+++ cli.js
@@ -1,42 +1,36 @@
 #!/usr/bin/env node
 
 var Blipper = require('./index.js')
-var _ = require('highland')
 
 var client = new Blipper()
 
-var command = process.argv[2] || 'help'
+const arg = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .command('name  <username>', 'Set your username')
+  .command('about <info>', 'Set your about info')
+  .command('profile', 'View our username, about, and unique node id')
+  .command('say <your_message>', 'Say something which your followers will see in their feeds')
+  .command('add <friend_id>', 'Add unique id of another node to follow')
+  .command('feed', 'Show your feed of your posts and posts by nodes you follow')
+  .help('h')
+  .alias('h', 'help')
+  .demandCommand(2)
+  .argv
 
-var commands = {
-  profile: function () {
-    return client.getProfile().map(function (profile) {
-      return 'name: ' + profile.username + '\n' +
-      'about: ' + profile.about + '\n' +
-      'id: ' + profile.id
-    })
-  },
-  name: client.setUsername,
-  about: client.setAbout,
-  say: client.post,
-  add: client.addFollowee,
-  feed: function () {
-    return client.getFeed().map(function (post) {
-      return (post.author.username || post.author.id) + ' - ' + (new Date(post.time)).toString() + '\n' +
-      '  ' + post.content + '\n'
-    })
-  },
-  help: function () {
-    return _.of(
-      'usage: blipper <command> [<arg>]\n' +
-      'commands:\n' +
-      '  name     Set your username\n' +
-      '  about    Set your about info\n' +
-      '  profile  View our username, about, and unique node id\n' +
-      '  say      Say something which your followers will see in their feeds\n' +
-      '  add      Add unique id of another node to follow\n' +
-      '  feed     Show your feed of your posts and posts by nodes you follow\n'
-    )
-  }
-}
+const commands = {}
 
-commands[command].apply(client, process.argv.slice(3)).invoke('toString').append('\n').pipe(process.stdout)
+commands.profile = () => client.getProfile().map(profile => `name: ${profile.username}
+about:  ${profile.about}
+id:  ${profile.id}
+`)
+
+commands.feed = client.getFeed().map(post => (`${post.author.username || post.author.id} - ${new Date(post.time)}
+${post.content}
+`))
+
+commands.name = client.setUsername
+commands.about = client.setAbout
+commands.say = client.post
+commands.add = client.addFollowee
+
+commands[arg._].apply(client, process.argv.slice(3)).invoke('toString').append('\n').pipe(process.stdout)
